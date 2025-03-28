01d949500f70acb014d2387a66c3792fb03328fe
Sophia Dolan <25srdolan@gmail.com>
2025-03-28 17:56:07 -0400

    Task 4:  Display products in the DOM with name, price, and image (first attempt)

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -39,3 +39,50 @@ async function fetchProductsAsync() {
     }   // handles error 
 }
 
+
+// Task 4:  Display products in the DOM with name, price, and image
+function displayProducts(products) {
+    const container = document.getElementById("product-container");
+  // Find the container of the products 
+    
+    container.innerHTML = "";
+    // Empties out the container
+  
+    
+    products.slice(0, 5).forEach(product => {
+    // takes the first 5 products 
+     
+      const { name, price, image } = product.fields;
+       // takes the name, price and image of product
+  
+      
+      const productDiv = document.createElement("div");
+      productDiv.className = "product";
+      // creating the HTML for each product
+  
+      
+      const productName = document.createElement("h2");
+      productName.textContent = name;
+      // Put the product name in an h2 tag
+  
+      
+      const img = document.createElement("img");
+      // creates image of the product
+      img.src = image[0].url; 
+      img.alt = name; 
+      //sets image and url 
+  
+    
+      const productPrice = document.createElement("p");
+      productPrice.textContent = `$${(price / 100).toFixed(2)}`; // Convert cents to dollars
+    // Adds prices
+      
+      productDiv.appendChild(productName);
+      productDiv.appendChild(img);
+      productDiv.appendChild(productPrice);
+      // Adds to the div
+
+      container.appendChild(productDiv);
+    }); // Puts the product div in container
+  }
+  
